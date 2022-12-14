b815785a3b7c670e269274b4d09cb8575690c3d5
juancho11gm <juansebastian11gm@gmail.com>
2022-12-14 16:21:40 -0500

    feat: 12

---
+++ 2022/12/12.ts
@@ -0,0 +1,30 @@
+import assert from 'assert';
+
+function selectSleigh(distance: number, sleighs: Sleigh[]) {
+  const battery = 20;
+  const sleigh = sleighs.reverse().find(sleigh => sleigh.consumption * distance <= battery);
+  return sleigh?.name || null;
+}
+
+type Sleigh = {
+  name: string;
+  consumption: number;
+}
+try {
+  const distance = 30
+  const sleighs = [
+    { name: "Dasher", consumption: 0.3 },
+    { name: "Dancer", consumption: 0.5 },
+    { name: "Rudolph", consumption: 0.7 },
+    { name: "Midu", consumption: 1 }
+  ]
+
+  assert.equal(selectSleigh(distance, sleighs), 'Dancer');
+  assert.equal(selectSleigh(50, [
+    { name: 'Pedrosillano', consumption: 1 },
+    { name: 'SamarJaffal', consumption: 2 },
+    { name: 'marcospage', consumption: 3 }
+  ]), null);
+} catch (error) {
+  console.log(error)
+}
