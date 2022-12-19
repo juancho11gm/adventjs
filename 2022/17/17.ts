e2daa86b49592d5f3ecb61bb583bafe5fe358da8
juancho11gm <juansebastian11gm@gmail.com>
2022-12-19 10:08:33 -0500

    feat: 2022 18

--- 2022/17/17.ts
+++ 2022/17/17.ts
@@ -1,8 +1,23 @@
 import assert from 'assert';
 
+function carryGifts(gifts: string[], maxWeight: number) {
+  const giftsStr = gifts.join(' ');
+  // \b for matching the start of a word
+  // \w * for matching the any word with 0 or more spaces
+  // {1, maxWeight} for matching between 1 and maxWeight characters
+  // (?= |$) for matching space or end without including them
+  const regex = new RegExp(`\\b(\\w *){1,${maxWeight}}(?= |$)`, 'g');
+  return giftsStr.match(regex) || [];
+}
 
 try {
-  assert.deepEqual();
+  assert.deepEqual(carryGifts(['game', 'bike', 'book', 'toy'], 10), ['game bike', 'book toy']);
+  assert.deepEqual(carryGifts(['game', 'bike', 'book', 'toy'], 7), ['game', 'bike', 'book toy']);
+  assert.deepEqual(carryGifts(['game', 'bike', 'book', 'toy'], 4), ['game', 'bike', 'book', 'toy']);
+  assert.deepEqual(carryGifts(['toy', 'gamme', 'toy', 'bike'], 6), ['toy', 'gamme', 'toy', 'bike']);
+  assert.deepEqual(carryGifts(['toy', 'toy', 'toy', 'toy'], 2), []);
+
+
 } catch (error) {
   console.log(error)
 }
--- 2022/18/18.ts
+++ 2022/18/18.ts
@@ -1,23 +1,13 @@
 import assert from 'assert';
 
-function carryGifts(gifts: string[], maxWeight: number) {
-  const giftsStr = gifts.join(' ');
-  // \b for matching the start of a word
-  // \w * for matching the any word with 0 or more spaces
-  // {1, maxWeight} for matching between 1 and maxWeight characters
-  // (?= |$) for matching space or end without including them
-  const regex = new RegExp(`\\b(\\w *){1,${maxWeight}}(?= |$)`, 'g');
-  return giftsStr.match(regex) || [];
+function dryNumber(dry: number, numbers: number) {
+  const arr = Array.from({ length: numbers }, (_, index) => index + 1);
+  return arr.filter(n => n.toString().includes(`${dry}`));
 }
 
 try {
-  assert.deepEqual(carryGifts(['game', 'bike', 'book', 'toy'], 10), ['game bike', 'book toy']);
-  assert.deepEqual(carryGifts(['game', 'bike', 'book', 'toy'], 7), ['game', 'bike', 'book toy']);
-  assert.deepEqual(carryGifts(['game', 'bike', 'book', 'toy'], 4), ['game', 'bike', 'book', 'toy']);
-  assert.deepEqual(carryGifts(['toy', 'gamme', 'toy', 'bike'], 6), ['toy', 'gamme', 'toy', 'bike']);
-  assert.deepEqual(carryGifts(['toy', 'toy', 'toy', 'toy'], 2), []);
-
-
+  assert.deepEqual(dryNumber(1, 15), [1, 10, 11, 12, 13, 14, 15])
+  assert.deepEqual(dryNumber(2, 20), [2, 12, 20])
 } catch (error) {
   console.log(error)
 }
