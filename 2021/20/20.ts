779eae353162fadd08be21d257310b1cb08b432a
juancho11gm <juansebastian11gm@gmail.com>
2022-12-28 14:06:50 -0500

    feat: 2021: 20

--- 2021/20/20.ts
+++ 2021/20/20.ts
@@ -1,7 +1,16 @@
 import assert from 'assert';
 
-try {
+function pangram(letter: string) {
+  const ALPHABET_LETTER_COUNT = 26;
+  const matches = letter.toLowerCase().match(/[a-zñ]/g);
+  return new Set(matches).size > ALPHABET_LETTER_COUNT;
+}
 
+try {
+  assert.equal(pangram('Extraño pan de col y kiwi se quemó bajo fugaz vaho'), true)
+  assert.equal(pangram('Jovencillo emponzoñado y con walkman: ¡qué figurota exhibes!'), true)
+  assert.equal(pangram('Esto es una frase larga pero no tiene todas las letras del abecedario'), false)
+  assert.equal(pangram('De la a a la z, nos faltan letras'), false)
 } catch (error) {
   console.log(error)
 }
