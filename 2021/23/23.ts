cac618f4ce32a2e983e04c6a396c0c9be418914b
juancho11gm <juansebastian11gm@gmail.com>
2022-12-29 11:21:48 -0500

    feat: 2021: 23

--- 2021/23/23.ts
+++ 2021/23/23.ts
@@ -1,7 +1,32 @@
 import assert from 'assert';
 
-try {
+function canReconfigure(from: string, to: string) {
+  if (from.length !== to.length) return false;
+  const map: Dict = {};
+  const letters = from.split('');
+  for (const [index, letter] of letters.entries()) {
+    if (!map[letter] && !map[to[index]]) {
+      map[letter] = to[index];
+      map[to[index]] = letter;
+    } else if (map[letter] !== to[index]) return false;
+  }
+  return true;
+}
 
+type Dict = {
+  [key: string]: string
+}
+
+try {
+  // B -> L
+  // A -> I
+  // L -> B
+  assert.equal(canReconfigure('BAL', 'LIB'), true);
+  assert.equal(canReconfigure('CON', 'JUU'), false);
+  assert.equal(canReconfigure('XBOX', 'XXBO'), false);
+  assert.equal(canReconfigure('XBOX', 'XOBX'), true);
+  assert.equal(canReconfigure('MMM', 'MID'), false);
+  assert.equal(canReconfigure('AA', 'MID'), false);
 } catch (error) {
   console.log(error)
 }
