dd4e03feac70e6e898aa7cb87ec507bbd883ccdf
juancho11gm <juansebastian11gm@gmail.com>
2022-12-29 11:45:16 -0500

    feat: 2021: 24

--- 2021/24/24.ts
+++ 2021/24/24.ts
@@ -1,7 +1,37 @@
 import assert from 'assert';
 
-try {
+function checkIsSameTree(treeA: Tree, treeB: Tree): boolean {
+  const { left: leftA, right: rightA, value: valueA } = treeA;
+  const { left: leftB, right: rightB, value: valueB } = treeB;
+  let leftEq = true;
+  let rightEq = true;
+  if (leftA && leftB) leftEq = checkIsSameTree(leftA, leftB);
+  if (rightA && rightB) rightEq = checkIsSameTree(rightA, rightB);
+  return valueA === valueB && leftEq && rightEq;
+}
 
+type Tree = {
+  value: number;
+  left: Tree | null;
+  right: Tree | null;
+}
+
+try {
+  const tree = {
+    value: 1,
+    left: { value: 2, left: null, right: null },
+    right: { value: 3, left: null, right: null }
+  }
+
+  const tree2 = {
+    value: 1,
+    left: { value: 3, left: { value: 2, left: null, right: null }, right: null },
+    right: { value: 5, left: null, right: { value: 4, left: null, right: null } }
+  }
+
+  assert.equal(checkIsSameTree(tree, tree), true);
+  assert.equal(checkIsSameTree(tree, tree2), false);
+  assert.equal(checkIsSameTree(tree2, tree2), true);
 } catch (error) {
   console.log(error)
 }
